f7886ef4964148394554d9795962a22f234317fa
lxy764139720 <764139720@qq.com>
2020-02-04 00:27:55 +0800

    创建项目 项目配置

---
+++ conf_res_web/src/main/java/com/project/conf_res/global/EncodingFilter.java
@@ -0,0 +1,25 @@
+package com.project.conf_res.global;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+
+    private String encoding = "UTF-8";
+
+    public void init(FilterConfig filterConfig) {
+        if (filterConfig.getInitParameter("ENCODING") != null) {
+            this.encoding = filterConfig.getInitParameter("ENCODING");
+        }
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(this.encoding);
+        servletResponse.setCharacterEncoding(this.encoding);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+        this.encoding = null;
+    }
+}
---
+++ src/main/java/com/project/conf_res/ConfResApplication.java
@@ -0,0 +1,13 @@
+package com.project.conf_res;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfResApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfResApplication.class, args);
+    }
+
+}
